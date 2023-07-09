3c613b6da4afba919d3df7b835b9d9217228428a
KuldeepSingh <in.singhkuldeep@outlook.com>
2023-07-09 23:00:32 +0100

    Load data from SQL Database on page load

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using ExcelDataReader;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using ExcelDataReader;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
@@ -7,189 +8,262 @@ using System.Text;
 
 namespace H2Input.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
-        private string dbtable = "dbo.H2Input1";
-        private string connectionString;
-        public DataTable capexData { get; set; }
-        public DataTable opexData { get; set; }
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		private string dbtable = "dbo.KD01";
+		private string connectionString;
+		public DataTable cat01Data { get; set; }
+		public DataTable cat02Data { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
-        {
-            _logger = logger;
-            //connectionString = $"Server={server};Database={database};User Id={user};Password={password};";
-            connectionString = configuration.GetConnectionString("DefaultConnection");
-        }
+		public DataTable UploadedDataTable { get; set; }
 
-        [BindProperty]
-        public Microsoft.AspNetCore.Http.IFormFile Upload { get; set; }
+		public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
 
-        public string Message { get; set; }
+		[BindProperty]
+		public Microsoft.AspNetCore.Http.IFormFile Upload { get; set; }
+
+		public string Message { get; set; }
 
 
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+		public async Task<IActionResult> OnGet()
+		{
+			HashSet<string> columnList = new HashSet<string>();
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
 
-        //public void OnGet()
-        //{
-        //    Message = TempData["Message"] as string;
-        //}
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (Upload != null)
-            {
-                try
-                {
-                    string fileName = Path.GetFileName(Upload.FileName);
-                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-                    string targetSheetName = "Summary";
-                    // Define a custom encoding provider that supports the required encoding
-                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+				string sql = $"SELECT * FROM {dbtable}";
+				SqlCommand command = new SqlCommand(sql, connection);
 
-                    using (var stream = new MemoryStream())
-                    {
-                        TempData["Message"] = "File Uploading ...";
+				SqlDataReader reader = command.ExecuteReader();
 
-                        Upload.CopyTo(stream);
-                        stream.Position = 0;
+				UploadedDataTable = new DataTable("UploadedData");
+				UploadedDataTable.Columns.Add("CategoryName", typeof(string));
+				UploadedDataTable.Columns.Add("ParameterName", typeof(string));
+				UploadedDataTable.Columns.Add("CaseName", typeof(string));
+				UploadedDataTable.Columns.Add("ParameterKey", typeof(string));
+				UploadedDataTable.Columns.Add("ParameterValue", typeof(string));
 
-                        // Create an ExcelDataReader for the uploaded file
-                        using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream, new ExcelReaderConfiguration()
-                        {
-                            FallbackEncoding = Encoding.GetEncoding(1252) // Specify the required encoding here
-                        }))
-                        {
-                            // Configure the reader to read the first sheet of the workbook
-                            var excelDataSetConfiguration = new ExcelDataSetConfiguration
-                            {
-                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration
-                                {
-                                    UseHeaderRow = true
-                                }
-                            };
+				while (reader.Read())
+				{
+					DataRow row = UploadedDataTable.NewRow();
+					row["CategoryName"] = reader["CategoryName"];
+					row["ParameterName"] = reader["ParameterName"];
+					row["CaseName"] = reader["CaseName"];
+					row["ParameterKey"] = reader["ParameterKey"];
+					row["ParameterValue"] = reader["ParameterValue"];
 
-                            // Load the Excel data into a DataSet
-                            var dataSet = reader.AsDataSet(excelDataSetConfiguration);
+					if (reader["CaseName"] is null || string.IsNullOrEmpty(reader["CaseName"].ToString()))
+					{
+						columnList.Add($"{reader["ParameterKey"]}");
+					}
+					else
+					{
+						columnList.Add($"{reader["CaseName"]}-{reader["ParameterKey"]}");
+					}
 
-                            // Access the first table (sheet) in the DataSet
-                            var dataTable = dataSet.Tables[targetSheetName];
-                            List<KeyValuePair<string, string>> headerList = new List<KeyValuePair<string, string>>();
+					UploadedDataTable.Rows.Add(row);
 
-                            if (dataTable != null)
-                            {
-                                using (SqlConnection connection = new SqlConnection(connectionString))
-                                {
-                                    await connection.OpenAsync();
-                                    using (SqlCommand truncateCommand = new SqlCommand($"Truncate table {dbtable}", connection))
-                                    {
-                                        await truncateCommand.ExecuteNonQueryAsync();
-                                    }
+				}
+			}
 
-                                    string categoryName = string.Empty;
-                                    opexData = new DataTable();
-                                    opexData.Columns.Add("-");
-                                    capexData = new DataTable();
-                                    capexData.Columns.Add("-");
+			cat01Data = new DataTable();
+			cat01Data.Columns.Add("-");
+			cat02Data = new DataTable();
+			cat02Data.Columns.Add("-");
 
-                                    var rowCount = dataTable.Rows.Count;
-                                    var colCount = dataTable.Columns.Count;
+			foreach (var column in columnList)
+			{
+				cat01Data.Columns.Add(column);
+				cat02Data.Columns.Add(column);
+			}
 
-                                    string parameterName = "";
-                                    string parameterKey = "";
-                                    string parameterValue = "";
-                                    //float parameterValue = 0;
-                                    int casecount = 0;
+			//var cat01Row = cat01Data.NewRow();
 
-                                    int col = 0;
+			//foreach (Row rowItem in UploadedDataTable.Rows)
+			//{
+				
+			//}
 
-                                    for (int row = 0; row < rowCount; row++)
-                                    {
-                                        col = 0;
-                                        casecount = 0;
 
-                                        if (categoryName.ToUpper() == "CAPEX" && headerList.Count() == 0)
-                                        {
-                                            col++;
-                                            while (col < colCount && !(dataTable.Rows[row][col] == DBNull.Value || string.IsNullOrEmpty(dataTable.Rows[row][col].ToString())))
-                                            {
-                                                if (col % 3 == 0)
-                                                {
-                                                    casecount++;
-                                                }
+			return Page();
+		}
 
-                                                capexData.Columns.Add($"Case {casecount} - {dataTable.Rows[row][col].ToString()}");
-                                                opexData.Columns.Add($"Case {casecount} - {dataTable.Rows[row][col].ToString()}");
+		//public void OnGet()
+		//{
+		//    Message = TempData["Message"] as string;
+		//}
 
-                                                KeyValuePair<string, string> pair = new KeyValuePair<string, string>(dataTable.Rows[row][col].ToString(), $"Case {casecount}");
-                                                headerList.Add(pair);
-                                                col++;
-                                            }
-                                        }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (Upload != null)
+			{
+				try
+				{
+					string fileName = Path.GetFileName(Upload.FileName);
+					string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+					string targetSheetName = "Summary";
+					// Define a custom encoding provider that supports the required encoding
+					Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-                                        if (dataTable.Rows[row][0] == DBNull.Value || string.IsNullOrEmpty(dataTable.Rows[row][0].ToString()))
-                                        {
-                                            continue;
-                                        }
-                                        else if (dataTable.Rows[row][0].ToString().ToUpper() == "CAPEX" || dataTable.Rows[row][0].ToString().ToUpper() == "OPEX")
-                                        {
-                                            categoryName = dataTable.Rows[row][0].ToString();
-                                            continue;
-                                        }
-                                        else
-                                        {
-                                            parameterName = dataTable.Rows[row][0].ToString();
-                                            col++;
-                                            var capexRow = capexData.NewRow();
-                                            var opexRow = opexData.NewRow();
-                                            capexRow[0] = parameterName;
-                                            opexRow[0] = parameterName;
+					using (var stream = new MemoryStream())
+					{
+						TempData["Message"] = "File Uploading ...";
 
-                                            for (int i = 0; i < headerList.Count; i++)
-                                            {
-                                                KeyValuePair<string, string> pair = headerList[i];
-                                                parameterValue = dataTable.Rows[row][i + 1].ToString();
-                                                capexRow[i + 1] = parameterValue;
-                                                opexRow[i + 1] = parameterValue;
+						Upload.CopyTo(stream);
+						stream.Position = 0;
 
-                                                string insertQuery = $"INSERT INTO {dbtable} (CategoryName, ParameterName, CaseName, ParameterKey, ParameterValue) " +
-                                                $"VALUES ('{categoryName}', '{parameterName}', '{pair.Value}', '{pair.Key}', '{parameterValue}')";
+						// Create an ExcelDataReader for the uploaded file
+						using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream, new ExcelReaderConfiguration()
+						{
+							FallbackEncoding = Encoding.GetEncoding(1252) // Specify the required encoding here
+						}))
+						{
+							// Configure the reader to read the first sheet of the workbook
+							var excelDataSetConfiguration = new ExcelDataSetConfiguration
+							{
+								ConfigureDataTable = (_) => new ExcelDataTableConfiguration
+								{
+									UseHeaderRow = true
+								}
+							};
 
-                                                using (SqlCommand command = new SqlCommand(insertQuery, connection))
-                                                {
-                                                    await command.ExecuteNonQueryAsync();
-                                                }
-                                                col++;
+							// Load the Excel data into a DataSet
+							var dataSet = reader.AsDataSet(excelDataSetConfiguration);
 
-                                            }
-                                            if (categoryName == "CAPEX")
-                                                capexData.Rows.Add(capexRow);
-                                            if (categoryName == "OPEX")
-                                                opexData.Rows.Add(opexRow);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+							// Access the first table (sheet) in the DataSet
+							var dataTable = dataSet.Tables[targetSheetName];
+							List<KeyValuePair<string, string>> headerList = new List<KeyValuePair<string, string>>();
 
-                    TempData["Message"] = "Excel file uploaded successfully.";
-                }
-                catch (Exception ex)
-                {
-                    TempData["Message"] = "An error occurred: " + ex.Message;
-                }
-            }
-            else
-            {
-                TempData["Message"] = "Please upload a valid Excel file.";
-            }
+							if (dataTable != null)
+							{
+								using (SqlConnection connection = new SqlConnection(connectionString))
+								{
+									await connection.OpenAsync();
+									using (SqlCommand truncateCommand = new SqlCommand($"Truncate table {dbtable}", connection))
+									{
+										await truncateCommand.ExecuteNonQueryAsync();
+									}
 
-            //return RedirectToPage("Index");
-            return Page();
-        }
-    }
+									string categoryName = string.Empty;
+									cat02Data = new DataTable();
+									cat02Data.Columns.Add("-");
+									cat01Data = new DataTable();
+									cat01Data.Columns.Add("-");
+
+									var rowCount = dataTable.Rows.Count;
+									var colCount = dataTable.Columns.Count;
+
+									string parameterName = "";
+									string parameterKey = "";
+									string parameterValue = "";
+									//float parameterValue = 0;
+									int casecount = 0;
+
+									int col = 0;
+
+									for (int row = 0; row < rowCount; row++)
+									{
+										col = 0;
+										casecount = 0;
+
+										if (categoryName == "Category01" && headerList.Count() == 0)
+										{
+											col++;
+											while (col < colCount && !(dataTable.Rows[row][col] == DBNull.Value || string.IsNullOrEmpty(dataTable.Rows[row][col].ToString())))
+											{
+												if (col % 3 == 0)
+												{
+													casecount++;
+												}
+
+												if (casecount < 1)
+												{
+													cat01Data.Columns.Add($"{dataTable.Rows[row][col].ToString()}");
+													cat02Data.Columns.Add($"{dataTable.Rows[row][col].ToString()}");
+													KeyValuePair<string, string> pair = new KeyValuePair<string, string>(dataTable.Rows[row][col].ToString(), $"");
+													headerList.Add(pair);
+												}
+												else
+												{
+													cat01Data.Columns.Add($"Case {casecount} - {dataTable.Rows[row][col].ToString()}");
+													cat02Data.Columns.Add($"Case {casecount} - {dataTable.Rows[row][col].ToString()}");
+													KeyValuePair<string, string> pair = new KeyValuePair<string, string>(dataTable.Rows[row][col].ToString(), $"Case {casecount}");
+													headerList.Add(pair);
+												}
+
+
+
+												col++;
+											}
+										}
+
+										if (dataTable.Rows[row][0] == DBNull.Value || string.IsNullOrEmpty(dataTable.Rows[row][0].ToString()))
+										{
+											continue;
+										}
+										else if (dataTable.Rows[row][0].ToString() == "Category01" || dataTable.Rows[row][0].ToString() == "Category02")
+										{
+											categoryName = dataTable.Rows[row][0].ToString();
+											continue;
+										}
+										else
+										{
+											parameterName = dataTable.Rows[row][0].ToString();
+											col++;
+											var capexRow = cat01Data.NewRow();
+											var opexRow = cat02Data.NewRow();
+											capexRow[0] = parameterName;
+											opexRow[0] = parameterName;
+
+											for (int i = 0; i < headerList.Count; i++)
+											{
+												KeyValuePair<string, string> pair = headerList[i];
+												parameterValue = dataTable.Rows[row][i + 1].ToString();
+												capexRow[i + 1] = parameterValue;
+												opexRow[i + 1] = parameterValue;
+
+												string insertQuery = $"INSERT INTO {dbtable} (CategoryName, ParameterName, CaseName, ParameterKey, ParameterValue) " +
+												$"VALUES ('{categoryName}', '{parameterName}', '{pair.Value}', '{pair.Key}', '{parameterValue}')";
+
+												using (SqlCommand command = new SqlCommand(insertQuery, connection))
+												{
+													await command.ExecuteNonQueryAsync();
+												}
+												col++;
+
+											}
+											if (categoryName == "Category01")
+												cat01Data.Rows.Add(capexRow);
+											if (categoryName == "Category02")
+												cat02Data.Rows.Add(opexRow);
+										}
+									}
+								}
+							}
+						}
+					}
+
+					TempData["Message"] = "Excel file uploaded successfully.";
+				}
+				catch (Exception ex)
+				{
+					TempData["Message"] = "An error occurred: " + ex.Message;
+				}
+			}
+			else
+			{
+				TempData["Message"] = "Please upload a valid Excel file.";
+			}
+
+			//return RedirectToPage("Index");
+			return Page();
+		}
+	}
 }
